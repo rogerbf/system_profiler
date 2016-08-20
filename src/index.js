e2fe2fefe53c1705fddaa3ead09e968793f9a74c
rogerbf <roger@bergfalk.nu>
2016-08-21 01:06:31 +0200

    seems to be working parser

--- src/index.js
+++ src/index.js
@@ -3,28 +3,64 @@ const inspect = require('util').inspect
 
 system_profiler('SPUSBDataType')
   .then(data => {
-
-    const processed = data
+    const processed = (
+    '{'.concat(data
       .split('\n')
-      .reduce((accumulator, line, i, arr) => {
-        if (line.length > 0) {
-          if (line[line.length - 1] === ':') {
-            return accumulator.concat(
-              `"${accumulator[accumulator.length - 1] === '\n' ? '}' : ''}${line.trim().slice(0, -1)}" : {`
-              )
+      .filter(line => line.length > 0)
+      .map(line => separateAttributes(line))
+      .reduce((accumulator, current, i, arr) => {
+        if (i === 0) {
+          if (current.parent) {
+            return `"${current.key}": {`
+          } else {
+            return `{ "${current.key}" : "${current.value}" }`
           }
-          else {
-            return accumulator.concat(
-              `"${line.slice(0, line.indexOf(':')).trim()}":"${line.slice(line.indexOf(':') + 1).trim()}",`
-            )
+        } else {
+          const previous = arr[i - 1]
+          const next = arr[i + 1]
+          if (previous.parent) {
+            if (current.parent) {
+              return accumulator.concat(`"${current.key}": {`)
+            } else {
+                if (next !== undefined && next.parent) {
+                  return accumulator.concat(`"${current.key}" : "${current.value}"`)
+                } else {
+                  return accumulator.concat(`"${current.key}" : "${current.value}",`)
+                }
+            }
+          } else {
+            if (current.parent) {
+              return accumulator.concat(`}, "${current.key}" : {`)
+            } else {
+                if (next !== undefined && next.parent) {
+                  return accumulator.concat(`"${current.key}" : "${current.value}"`)
+                } else {
+                  return accumulator.concat(`"${current.key}" : "${current.value}",`)
+                }
+            }
           }
         }
-        else {
-          return accumulator
-        }
-      }, '').concat('}')
-
-    console.log(processed)
-
+      }, '').slice(0, -1).concat('}}')).concat('}')
+    )
+    console.log(JSON.stringify(JSON.parse(processed), null, 2))
   })
   .catch(err => console.error(error))
+
+const separateAttributes = line => {
+  if (line[line.length - 1] !== ':') {
+    return {
+      indentation: line.slice(0, line.match(/\w/).index).length,
+      key: line.slice(0, line.indexOf(':')).trim(),
+      value: line.slice(line.indexOf(':') + 1).trim(),
+      parent: false
+    }
+  } else {
+    return {
+      indentation: line.slice(0, line.match(/\w/).index).length,
+      key: line.slice(0, line.indexOf(':')).trim(),
+      value: {},
+      parent: true,
+      children: []
+    }
+  }
+}
